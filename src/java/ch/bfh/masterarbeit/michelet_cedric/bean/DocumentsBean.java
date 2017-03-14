1deb1d38c0de68900d034e3ff4ba40c8c6ad921b
michelet <michelet@mb-tb-cmi.home>
2017-03-14 18:27:50 +0100

    Load a list of sample documents.

--- src/java/ch/bfh/masterarbeit/michelet_cedric/bean/DocumentsBean.java
+++ src/java/ch/bfh/masterarbeit/michelet_cedric/bean/DocumentsBean.java
@@ -5,6 +5,7 @@
  */
 package ch.bfh.masterarbeit.michelet_cedric.bean;
 import ch.bfh.masterarbeit.michelet_cedric.model.CDADocument;
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -20,6 +21,19 @@ public class DocumentsBean {
     
     public DocumentsBean() {
         documents = new ArrayList<>();
+        this._loadSampleData();
     }
     
+    /**
+     * Load a list of sample documents
+     */
+    private void _loadSampleData() {
+        try {
+            documents.add(new CDADocument("1", "test 1.1", CDADocument.getSampleContent("sample1.xml")));
+            documents.add(new CDADocument("2", "test 2", "body"));
+            documents.add(new CDADocument("3", "test 3", "body"));
+        } catch(IOException e) {
+            //@TODO handle error
+        }
+    }
 }
