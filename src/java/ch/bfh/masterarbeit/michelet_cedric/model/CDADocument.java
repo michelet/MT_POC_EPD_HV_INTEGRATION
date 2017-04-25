38c93ce55f911f25492c2708bf2cc4c9dc4ffa70
michelet <michelet@mb-tb-cmi.home>
2017-04-25 19:22:06 +0200

    Import class CDA wrapper.

--- src/java/ch/bfh/masterarbeit/michelet_cedric/model/CDADocument.java
+++ src/java/ch/bfh/masterarbeit/michelet_cedric/model/CDADocument.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ch.bfh.masterarbeit.michelet_cedric.model;
 
 import java.io.BufferedReader;
